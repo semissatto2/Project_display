4f3f1605dab85596feb6fc656f818585a12767d4
Guilherme Semissatto <semissatto@gmail.com>
2016-07-26 15:19:51 -0300

    Update faztudo.sh

--- faztudo.sh
+++ faztudo.sh
@@ -3,25 +3,32 @@ sudo ntpdate pool.ntp.org
 sudo apt-get update
 sudo apt-get install build-essential python-dev python-setuptools python-pip python-smbus -y
 sudo pip install Adafruit_BBIO
-sudo apt-get install python-pygame –y
+sudo apt-get install python-pygame -y
 cd /home/debian/Desktop/
 git clone https://github.com/semissatto2/Project_display.git
 cd /home/debian/Desktop/Project_display/
 sudo chmod 777 GPIO_callback4_sem_browser.py
 sudo chmod 777 GPIO_callback4.py
-sudo chmod 777 myserv.sh
-sudo cp myserv.sh /etc/init.d
-sudo /etc/init.d/myserv.sh start
+sudo chmod 777 display.sh
+sudo chmod 777 launcher.service
+sudo cp display.sh /usr/bin/display.sh
+sudo cp launcher.service /lib/systemd/launcher.service
+ln -s /lib/systemd/launcher.service /etc/systemd/system/launcher.service
 systemctl daemon-reload
+systemctl enable /lib/systemd/launcher.service
 sudo apt-get install xdotool -y
 sudo apt-get install unclutter -y
 cd /home/debian/Desktop/Project_display/
 sudo chmod 777 atualiza.sh
-sudo cp atualiza.sh /usr/bin/
+sudo cp atualiza.sh /usr/bin/atualiza.sh
 sudo mkdir /home/debian/.config/autostart/
 cd /home/debian/Desktop/Project_display/
 sudo chmod 777 autoscript.desktop
 sudo cp autoscript.desktop /home/debian/.config/autostart/autoscript.desktop
+systemctl start launcher.service
+
+
+
 
 #Script de configuração completa da BBB 
 #Tornar este arquivo executável com $sudo chmod 777 faztudo.sh
