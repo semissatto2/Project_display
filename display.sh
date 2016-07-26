3d29b9eb0b6fc0dd03b39b71d5c0bfc7defd6e8b
Guilherme Semissatto <semissatto@gmail.com>
2016-07-26 15:02:16 -0300

    Create display.sh

---
+++ display.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+python /home/debian/Desktop/Project_display/GPIO_callback4.py
+
+#Script de execucao do codigo Python. Trocar para python /home/debian/Desktop/Project_display/GPIO_callback4_sem_browser.py para versao sem browser
