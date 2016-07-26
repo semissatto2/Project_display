85f48be268fb4a5de09f080f5ed2f73fbee520c2
Guilherme Semissatto <semissatto@gmail.com>
2016-07-26 13:01:12 -0300

    Update faztudo.sh

--- faztudo.sh
+++ faztudo.sh
@@ -4,9 +4,9 @@ sudo apt-get update
 sudo apt-get install build-essential python-dev python-setuptools python-pip python-smbus -y
 sudo pip install Adafruit_BBIO
 sudo apt-get install python-pygame –y
-cd /home/Debian/Desktop/
+cd /home/debian/Desktop/
 git clone https://github.com/semissatto2/Project_display.git
-cd /home/Debian/Desktop/Project_display/
+cd /home/debian/Desktop/Project_display/
 sudo chmod 777 GPIO_callback4_sem_browser.py
 sudo chmod 777 GPIO_callback4.py
 sudo chmod 777 myserv.sh
@@ -15,11 +15,11 @@ sudo /etc/init.d/myserv.sh start
 systemctl daemon-reload
 sudo apt-get install xdotool -y
 sudo apt-get install unclutter -y
-cd /home/Debian/Desktop/Project_display/
+cd /home/debian/Desktop/Project_display/
 sudo chmod 777 atualiza.sh
 sudo cp atualiza.sh /usr/bin/
 sudo mkdir /home/debian/.config/autostart/
-cd /home/Debian/Desktop/Project_display/
+cd /home/debian/Desktop/Project_display/
 sudo chmod 777 autoscript.desktop
 sudo cp autoscript.desktop /home/debian/.config/autostart/autoscript.desktop
 
