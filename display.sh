548075431e2c8785ba8de97b43b386fc73b22391
Guilherme Semissatto <semissatto@gmail.com>
2016-07-29 14:58:59 -0300

    Update display.sh

--- display.sh
+++ display.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
-
 python /home/debian/Desktop/Project_display/GPIO_callback4.py
+
 
 #Script de execucao do codigo Python. 
 #Trocar para python /home/debian/Desktop/Project_display/GPIO_callback4_sem_browser.py para versao sem browser.
 #Deve ser colocado em /usr/bin/ 
+  
