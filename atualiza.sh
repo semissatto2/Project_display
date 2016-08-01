a76094d466e7799ae3ac20a8605d8eebb6dfbce1
Guilherme Semissatto <semissatto@gmail.com>
2016-08-01 15:55:09 -0300

    Update atualiza.sh

--- atualiza.sh
+++ atualiza.sh
@@ -11,9 +11,6 @@ sudo cp /home/debian/Desktop/shared/falha.jpg /home/debian/Desktop/Project_displ
 
 sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' /home/debian/.config/chromium/Default/Preferences
 chromium --kiosk --disable-infobars --disable-session-crashed-bubble http://10.2.105.115:8080/LinhaInfoWeb/
-sudo sleep 25s
-sudo xdotool search --class Chromium windowactivate
-sudo xdotool key F5
 unclutter -idle 1 -root
 echo "press ctrl c to stop"
 exit 0
