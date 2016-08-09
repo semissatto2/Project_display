effa9b257b5de18809488f13c0eaadb243a7e18c
Guilherme Semissatto <semissatto@gmail.com>
2016-08-09 13:03:04 -0300

    Update atualiza.sh

--- atualiza.sh
+++ atualiza.sh
@@ -3,11 +3,6 @@ export GOOGLE_API_KEY="no"
 export GOOGLE_DEFAULT_CLIENT_ID="no"
 export GOOGLE_DEFAULT_CLIENT_SECRET="no"
 
-sudo mkdir /home/debian/Desktop/shared
-sudo cp /home/debian/Desktop/shared/beamon.jpg /home/debian/Desktop/Project_display/beamon.jpg
-sudo cp /home/debian/Desktop/shared/beamoff.jpg /home/debian/Desktop/Project_display/beamoff.jpg
-sudo cp /home/debian/Desktop/shared/imminent.jpg /home/debian/Desktop/Project_display/imminent.jpg
-sudo cp /home/debian/Desktop/shared/falha.jpg /home/debian/Desktop/Project_display/falha.jpg
 
 sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' /home/debian/.config/chromium/Default/Preferences
 chromium --kiosk --disable-infobars --disable-session-crashed-bubble http://10.2.105.115:8080/LinhaInfoWeb/
