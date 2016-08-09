f5839003f5a78aa5dbbba2ad8e4d5cf0afeae2fd
Guilherme Semissatto <semissatto@gmail.com>
2016-08-09 13:05:15 -0300

    Update atualiza2.sh

--- atualiza2.sh
+++ atualiza2.sh
@@ -5,7 +5,10 @@ for i in {0..4..2}
     sleep 20s
     sudo xdotool search --class Chromium windowactivate
     sudo xdotool key F5
+    sudo mkdir /home/debian/Desktop/shared
     mount.cifs //centaurus/TROCA/gsemissatto/BBB /home/debian/Desktop/shared/ -o credentials=/home/debian/Desktop/Project_display/pwd.txt
+    sudo cp /home/debian/Desktop/shared/* /home/debian/Desktop/Project_display/images/
+
   done
 exit 0
 
