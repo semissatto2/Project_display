1c24e1372025009df9e72963330c1ec530892485
Guilherme Semissatto <semissatto@gmail.com>
2016-11-03 11:19:13 -0200

    Update faztudo.sh

--- faztudo.sh
+++ faztudo.sh
@@ -1,51 +1,73 @@
 #!/bin/sh
+green=`tput setaf 1`
+
+echo "${green}Atualizando data do sistema..."
 sudo ntpdate pool.ntp.org
+echo "${green}Data atualizada"
+
+echo "${green}Atualizando Debian e obtendo pacotes Python pip, Adafruit, Pygame, Xdotool, Unclutter, Samba..."
 sudo apt-get update
 sudo apt-get install build-essential python-dev python-setuptools python-pip python-smbus -y
 sudo pip install Adafruit_BBIO
 sudo apt-get install python-pygame -y
+sudo apt-get install xdotool -y
+sudo apt-get install unclutter -y
+aptitude install cifs-utils -y
+echo "${green}Debian atualizado. Todos pacotes foram obtidos com sucesso..."
+sleep 5
+
+echo "${green}Clonando Repositório..."
 cd /home/debian/Desktop/
 git clone https://github.com/semissatto2/Project_display.git
+echo "${green}Repositório clonado com sucesso..."
+sleep 5
+
+echo "${green}Concedendo permissões 777 às rotinas de automação..."
 cd /home/debian/Desktop/Project_display/
 sudo chmod 777 GPIO_callback4_sem_browser.py
 sudo chmod 777 GPIO_callback4.py
 sudo chmod 777 display.sh
 sudo chmod 777 launcher.service
 sudo chmod 777 atualiza.service
-cd /home/debian/Desktop/Project_display/
-cp display.sh /usr/bin/display.sh
-cd /home/debian/Desktop/Project_display/
-cp launcher.service /lib/systemd/launcher.service
-ln -s /lib/systemd/launcher.service /etc/systemd/system/launcher.service
-sudo systemctl daemon-reload
-sudo systemctl enable /lib/systemd/launcher.service
-cd /home/debian/Desktop/Project_display/
-cp atualiza.service /lib/systemd/atualiza.service
-ln -s /lib/systemd/atualiza.service /etc/systemd/system/atualiza.service
-sudo systemctl daemon-reload
-sudo systemctl enable /lib/systemd/atualiza.service
-sudo apt-get install xdotool -y
-sudo apt-get install unclutter -y
-cd /home/debian/Desktop/Project_display/
-sudo chmod 777 atualiza.sh
-cd /home/debian/Desktop/Project_display/
-sudo chmod 777 atualiza2.sh
-cd /home/debian/Desktop/Project_display/
-cp atualiza.sh /usr/bin/atualiza.sh
-cd /home/debian/Desktop/Project_display/
-cp atualiza2.sh /usr/bin/atualiza2.sh
-sudo mkdir /home/debian/.config/autostart/
-cd /home/debian/Desktop/Project_display/
 sudo chmod 777 autoscript.desktop
 sudo chmod 777 autoscript2.desktop
+sudo chmod 777 atualiza.sh
+sudo chmod 777 atualiza2.sh
+echo "${green}Permissões concedidas..."
+sleep 5
+
+echo "${green}Copiando arquivos..."
+cd /home/debian/Desktop/Project_display/
+cp display.sh /usr/bin/display.sh
+cp launcher.service /lib/systemd/launcher.service
+cd /home/debian/Desktop/Project_display/
+cp atualiza.service /lib/systemd/atualiza.service
+cp atualiza.sh /usr/bin/atualiza.sh
+cp atualiza2.sh /usr/bin/atualiza2.sh
+sudo mkdir /home/debian/.config/autostart/
 cd /home/debian/Desktop/Project_display/
 cp autoscript.desktop /home/debian/.config/autostart/autoscript.desktop
 cd /home/debian/Desktop/Project_display/
 cp autoscript2.desktop /home/debian/.config/autostart/autoscript2.desktop
-aptitude install cifs-utils -y
+echo "${green}Arquivos copiados..."
+sleep 3
+
+echo "${green}Incorporando scripts de automação ao sistema..."
+ln -s /lib/systemd/launcher.service /etc/systemd/system/launcher.service
+sudo systemctl daemon-reload
+sudo systemctl enable /lib/systemd/launcher.service
+ln -s /lib/systemd/atualiza.service /etc/systemd/system/atualiza.service
+sudo systemctl daemon-reload
+sudo systemctl enable /lib/systemd/atualiza.service
+echo "${green}Scripts incorporados ao sistema..."
+sleep 3
+
+
+
+echo "${green}Inicializando os scripts de automação..."
 sudo systemctl start launcher.service
 sudo systemctl start atualiza.service
-
+echo "${green}Beaglebone completamente configurada!"
 
 
 
