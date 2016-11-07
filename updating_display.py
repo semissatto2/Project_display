96dd63a68a09276a3daba01203ba08458b21bffa
root <root@gaebbb01.(none)>
2016-11-07 11:06:16 +0000

    Add updating.jpg

---
+++ updating_display.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import time
+import pygame
+from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+pygame.mouse.set_visible(0)
+image = pygame.image.load("/home/debian/Desktop/Project_display/updating.jpg")
+image = pygame.transform.scale(image, (screen.get_size()[0], screen.get_size()[1]))
+back = pygame.Surface(screen.get_size())
+back = back.convert()
+back.blit(image,(0,0))
+screen.blit(back,(0,0))
+	while True:
+                pygame.display.flip()
+        	time.sleep(0.5)
