526287cf03439faa57e38a54379b654c4786dac7
Guilherme Semissatto <semissatto@gmail.com>
2017-08-17 17:26:32 -0300

    Add files via upload

---
+++ displayWithoutBrowser.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import Adafruit_BBIO.GPIO as GPIO
+import time
+import pygame
+from pygame.locals import *
+from sys import exit
+
+
+
+#__GLOBAL_VARIABLES_CONTROL
+a = 1
+
+#__FUNCTIONS
+def funcao_0(channel):
+        time.sleep(0.03)
+        global a
+        a = a + 1
+        if a == 1001:
+                a = 1
+        if a % 2 == 0:
+            x = 8*GPIO.input("P8_18")+4*GPIO.input("P8_16")+2*GPIO.input("P8_14")+GPIO.input("P8_12")
+            print x
+
+def funcao_1():
+	'''image1 = pygame.image.load("/home/debian/Desktop/Project_display/images/beamon.png")
+	image1 = pygame.transform.scale(image1, (screen.get_size()[0], screen.get_size()[1]))
+	back1 = pygame.Surface(screen.get_size())
+	back1 = back1.convert()
+	back1.blit(image1,(0,0))
+	screen.blit(back1,(0,0))
+	#while GPIO.input("P8_11"):
+	pygame.display.flip()'''
+
+#__SETUP_GPIO
+
+GPIO.setup("P8_11", GPIO.IN)
+GPIO.add_event_detect("P8_11", GPIO.BOTH, callback=funcao_0, bouncetime=150)
+GPIO.setup("P8_12", GPIO.IN)
+GPIO.setup("P8_14", GPIO.IN)
+GPIO.setup("P8_16", GPIO.IN)
+GPIO.setup("P8_17", GPIO.IN)
+GPIO.setup("P8_18", GPIO.IN)
+
+#__SETUP_PYGAME
+pygame.init()
+screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+
+#__PERMANENT_LOOP
+while True:
+	time.sleep(1)
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			exit()
+		elif event.type == KEYDOWN:
+			if event.key == K_ESCAPE:
+				exit()
