2b007137151da077028998fb7c2abd6ee30b374e
Guilherme Semissatto <semissatto@gmail.com>
2017-08-22 09:58:21 -0300

    Adicionadas rotinas de try/except

--- displayWithBrowser.py
+++ displayWithBrowser.py
@@ -21,16 +21,16 @@ def funcao_0(channel):
 		screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
 		pygame.mouse.set_visible(0)
 		x = 8*GPIO.input("P8_18")+4*GPIO.input("P8_16")+2*GPIO.input("P8_14")+GPIO.input("P8_12")
-        directory_shared = "/home/debian/Desktop/shared/" + str(x) + ".png"
-        directory_interno = "/home/debian/Desktop/Project_display/images/" + str(x) + ".png"
+		directory_shared = "/home/debian/Desktop/shared/" + str(x) + ".png"
+		directory_interno = "/home/debian/Desktop/Project_display/images/" + str(x) + ".png"
 
 		# Tenta carregar a imagem do diretorio compartilhado. Caso nao consiga, carrega do diretorio interno
-        try:
-            print directory_shared
-            image = pygame.image.load(directory_shared)
+		try:
+			print directory_shared
+			image = pygame.image.load(directory_shared)
         except:
-            print directory_interno
-            image = pygame.image.load(directory_interno)
+			print directory_interno
+			image = pygame.image.load(directory_interno)
 
 		image = pygame.transform.scale(image, (screen.get_size()[0], screen.get_size()[1]))
 		back = pygame.Surface(screen.get_size())
