a6fc0877b6268bd67f97967a9ac4cf79bb073daf
Guilherme Semissatto <semissatto@gmail.com>
2018-05-23 10:46:45 -0300

    Update display.sh

--- display.sh
+++ display.sh
@@ -3,6 +3,6 @@ python /home/debian/Desktop/Project_display/displayWithoutBrowser.py
 
 
 #Script de execucao do codigo Python. 
-#Trocar para python /home/debian/Desktop/Project_display/GPIO_callback4_sem_browser.py para versao sem browser.
+#Trocar para python /home/debian/Desktop/Project_display/displayWithBrowser.py para versao sem browser.
 #Deve ser colocado em /usr/bin/ 
   
